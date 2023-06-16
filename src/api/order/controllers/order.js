ec273c749fcef8196a38267a9c9cd05e7b56a6bc
kunalshokeen051 <119399583+kunalshokeen051@users.noreply.github.com>
2023-06-16 13:55:14 +0530

    files added

--- src/api/order/controllers/order.js
+++ src/api/order/controllers/order.js
@@ -1,48 +1,9 @@
-("use strict");
-const stripe = require("stripe")(process.env.STRIPE_KEY);
+'use strict';
 
-const { createCoreController } = require("@strapi/strapi").factories;
+/**
+ * order controller
+ */
 
-module.exports = createCoreController("api::order.order", ({ strapi }) => ({
-  async create(ctx) {
-    const { products } = ctx.request.body;
-    try {
-      const lineItems = await Promise.all(
-        products.map(async (product) => {
-          const item = await strapi
-            .service("api::product.product")
-            .findOne(product.id);
+const { createCoreController } = require('@strapi/strapi').factories;
 
-          return {
-            price_data: {
-              currency: "inr",
-              product_data: {
-                name: item.title,
-              },
-              unit_amount: Math.round(item.price * 100),
-            },
-            quantity: product.attributes.quantity,
-          };
-        })
-      );
-
-      const session = await stripe.checkout.sessions.create({
-        shipping_address_collection: { allowed_countries: ["IN"] },
-        payment_method_types: ["card"],
-        mode: "payment",
-        success_url: process.env.CLIENT_URL + "/success",
-        cancel_url: process.env.CLIENT_URL + "?success=false",
-        line_items: lineItems,
-      });
-
-      await strapi
-        .service("api::order.order")
-        .create({ data: { products, stripeId: session.id } });
-
-      return { stripeSession: session };
-    } catch (error) {
-      ctx.response.status = 500;
-      return { error };
-    }
-  },
-}));
+module.exports = createCoreController('api::order.order');
